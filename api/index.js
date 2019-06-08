7806a6088ff837315581259cb439b11dacb24782
jazeng <zengja@oregonstate.edu>
2019-06-07 19:27:14 -0700

    Added create new user login get user and auth stuff

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 
 router.use('/courses', require('./courses'));
-
+router.use('/users', require('./users'));
 module.exports = router;
---
+++ api/users.js
@@ -0,0 +1,79 @@
+/*
+ * API routes for 'users' collection.
+ */
+
+const router = require('express').Router();
+
+const { validateAgainstSchema } = require('../lib/validation');
+const { generateAuthToken, requireAuthentication } = require('../lib/auth');
+const { UserSchema, insertNewUser, getUserById, validateUser, getUserByEmail } = require('../models/user');
+
+router.post('/', async (req, res) => {
+  if (validateAgainstSchema(req.body, UserSchema)) {
+    try {
+      const id = await insertNewUser(req.body);
+      res.status(201).send({
+        _id: id
+      });
+    } catch (err) {
+      console.error("  -- Error:", err);
+      res.status(500).send({
+        error: "Error inserting new user.  Try again later."
+      });
+    }
+  } else {
+    res.status(400).send({
+      error: "Request body does not contain a valid User."
+    });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  if (req.body && req.body.email && req.body.password) {
+    try {
+      const authenticated = await validateUser(req.body.email, req.body.password);
+      if (authenticated) {
+        const token = await generateAuthToken(req.body.email);
+        res.status(200).send({
+          token: token
+        });
+      } else {
+        res.status(401).send({
+          error: "Invalid credentials"
+        });
+      }
+    } catch (err) {
+      res.status(500).send({
+        error: "Error validating user.  Try again later."
+      });
+    }
+  } else {
+    res.status(400).send({
+      error: "Request body was invalid"
+    });
+  }
+});
+
+router.get('/:id', requireAuthentication, async (req, res, next) => {
+  if (req.params.id === req.user) {
+    try {
+      const user = await getUserById(req.params.id);
+      if (user) {
+        res.status(200).send(user);
+      } else {
+        next();
+      }
+    } catch (err) {
+      console.error("  -- Error:", err);
+      res.status(500).send({
+        error: "Error fetching user.  Try again later."
+      });
+    }
+  } else {
+    res.status(403).send({
+      error: "Unauthorized to access the specified resource"
+    });
+  }
+});
+
+module.exports = router;
